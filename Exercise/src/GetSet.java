b2c117000639db8cd59311a7a7c20cbda8838baf
dreamer2018 <zhoupans_mail@163.com>
2015-10-21 20:39:33 +0800

    add

---
+++ Exercise/src/GetSet.java
@@ -0,0 +1,30 @@
+/**
+ * Created by zhoupan on 10/21/15.
+ */
+public class GetSet {
+    private int id;
+    private String name;
+    private boolean sex;
+    private double score;
+
+    GetSet(){
+    }
+    GetSet(String name){
+        this.name=name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setScore(double score) {
+        this.score = score;
+    }
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+
+    public String toString(){
+        System.out.println("id=" + id + " name=" + name + " sex=" + (sex ? "男" : "女")+" score="+score);
+        return null;
+    }
+}
