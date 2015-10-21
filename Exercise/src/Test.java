175aca0ac7a9f8d04853d709e97d59f34bc7b749
dreamer2018 <zhoupans_mail@163.com>
2015-10-21 20:40:02 +0800

    add

---
+++ Exercise/src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created by zhoupan on 10/21/15.
+ */
+public class Test {
+    public static void main(String[] args){
+        GetSet gt= new GetSet("jake");
+        gt.setId(1001);
+        gt.setScore(90);
+        gt.setSex(true);
+        gt.toString();
+    }
+}
