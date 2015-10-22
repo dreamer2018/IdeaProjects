593d3e77189d133fbaaebf17baa5477ef5c2d07a
dreamer2018 <zhoupans_mail@163.com>
2015-10-22 09:35:37 +0800

    add

---
+++ Exercise/src/StaticTest.java
@@ -0,0 +1,21 @@
+/**
+ * Created by zhoupan on 10/21/15.
+ */
+public class StaticTest {
+    public static int Max(int a,int b){
+        if(a>=b){
+            return a;
+        }
+        else {
+            return b;
+        }
+    }
+    public int Min(int a,int b){
+        if(a>=b){
+            return b;
+        }
+        else{
+            return a;
+        }
+    }
+}
