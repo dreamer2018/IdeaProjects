d9ad3bdcf435c22b0a5338495458d795f1ccdb52
dreamer2018 <zhoupans_mail@163.com>
2015-11-09 09:19:57 +0800

    add

--- Exercise/src/Test.java
+++ Exercise/src/Test.java
@@ -1,8 +1,22 @@
+import java.net.StandardSocketOptions;
+
 /**
  * Created by zhoupan on 15-11-8.
  */
-public class Test {
+/*
+public class Test extends Person{
+
+    //创建一个静态初始化块
+    static {
+        System.out.print("这是Person的子类的初始化块\n");
+    }
     public static void main(String[] args){
-        Sub sb= new Sub();
+
+    }
+}
+*/
+public class Test{
+    public static void main(String[] args){
+        Leaf leaf=new Leaf();
     }
 }
