c1c1bfff2c6556eeb41b111f454c5cb25b035f11
dreamer2018 <zhoupans_mail@163.com>
2015-11-09 20:12:29 +0800

    add

--- Exercise/src/JavaQuestion2.java
+++ Exercise/src/JavaQuestion2.java
@@ -1,5 +1,31 @@
 /**
  * Created by zhoupan on 15-11-9 下午5:36.
  */
+
+import java.util.Scanner;
+
+class Stock{
+    String symbol;
+    String name;
+    double previousClosingPrice;
+    double currentPrice;
+
+    public Stock(String name, String symbol, double previousClosingPrice, double currentPrice) {
+        this.name = name;
+        this.symbol = symbol;
+        this.previousClosingPrice = previousClosingPrice;
+        this.currentPrice = currentPrice;
+    }
+
+    public double getChangePercent() {
+        return (currentPrice-previousClosingPrice)/previousClosingPrice;
+    }
+}
 public class JavaQuestion2 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        Stock s=new Stock(scanner.next(),scanner.next(),scanner.nextDouble(),scanner.nextDouble());
+        System.out.printf("%.2f%%",100*s.getChangePercent());
+    }
+
 }
