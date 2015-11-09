b53a14619bea8a21fdccd33e40c932d429845849
dreamer2018 <zhoupans_mail@163.com>
2015-11-09 09:19:17 +0800

    add

--- Exercise/src/StaticIInitTest.java
+++ Exercise/src/StaticIInitTest.java
@@ -2,4 +2,12 @@
  * Created by zhoupan on 15-11-9.
  */
 public class StaticIInitTest {
+    static int a=200;
+    static {
+        a=100;
+    }
+
+    public static void main(String[] args){
+        System.out.print("a="+a);
+    }
 }
