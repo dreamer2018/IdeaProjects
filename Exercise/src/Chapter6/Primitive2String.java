88834bcbb8e06e90852840e579c3b5a793354e66
dreamer2018 <zhoupans_mail@163.com>
2015-11-10 09:59:00 +0800

    add

---
+++ Exercise/src/Chapter6/Primitive2String.java
@@ -0,0 +1,25 @@
+package Chapter6;
+
+/**
+ * Created by zhoupan on 15-11-10 上午8:52.
+ */
+public class Primitive2String {
+    public static void main(String[] args) {
+
+        String intstr="123";
+        int int1=Integer.parseInt(intstr);
+        int int2=new Integer(intstr);
+        System.out.println("int1="+int1+",int2="+int2);
+        String str1=String.valueOf("123");
+        String str2=String.valueOf("false");
+        System.out.println("str1="+str1+"str2="+str2);
+        Integer a1=129;
+        Integer a2=128;
+        System.out.println(a1==a2);
+        System.out.println(a1.equals(a2));
+        System.out.println(Boolean.compare(true,false));
+        System.out.println(Boolean.compare(true,true));
+        System.out.println(Boolean.compare(false,true));
+    }
+
+}
---
+++ Exercise/src/Chapter6/Primitive2Wrapper.java
@@ -0,0 +1,7 @@
+package Chapter6;
+
+/**
+ * Created by zhoupan on 15-11-10 上午8:14.
+ */
+public class Primitive2Wrapper {
+}
