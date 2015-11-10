e05a1f098a1d3376a1f5fbaa16683ae4d50073c8
dreamer2018 <zhoupans_mail@163.com>
2015-11-10 15:36:03 +0800

    add

--- Exercise/src/Chapter6/NullAccessStatic.java
+++ Exercise/src/Chapter6/NullAccessStatic.java
@@ -1,7 +1,15 @@
-package Chapter6;
-
 /**
  * Created by zhoupan on 15-11-10 下午3:24.
  */
+package Chapter6;
 public class NullAccessStatic {
+    private static void test(){
+        System.out.println("NullAccessStatic类的类方法");
+    }
+
+    public static void main(String[] args) {
+        NullAccessStatic n=null;
+        n.test();
+       // n.toString();
+    }
 }
