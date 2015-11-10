509237f3b9698526c2e48f5ce6f5714ba257f81c
dreamer2018 <zhoupans_mail@163.com>
2015-11-10 15:36:39 +0800

    add

--- Exercise/src/Chapter6/PrintObject.java
+++ Exercise/src/Chapter6/PrintObject.java
@@ -1,7 +1,30 @@
-package Chapter6;
+
 
 /**
  * Created by zhoupan on 15-11-10 下午12:14.
  */
+package Chapter6;
+
+class Person{
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+    public void printInfo(){
+        System.out.println("此人名为"+name);
+    }
+
+    @Override
+    public String toString() {
+        return "对象为："+super.toString();
+    }
+}
 public class PrintObject {
+    public static void main(String[] args) {
+        Person person=new Person("xiaoming");
+        person.printInfo();
+        System.out.println(person.toString());
+        System.out.println(person);
+    }
 }
