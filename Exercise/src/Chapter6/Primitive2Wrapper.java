195d92ca5467d6f5a2c7d04fd4cfd669f77da0a9
dreamer2018 <zhoupans_mail@163.com>
2015-11-10 09:59:33 +0800

    add

--- Exercise/src/Chapter6/Primitive2Wrapper.java
+++ Exercise/src/Chapter6/Primitive2Wrapper.java
@@ -1,7 +1,18 @@
-package Chapter6;
-
 /**
  * Created by zhoupan on 15-11-10 上午8:14.
  */
+package Chapter6;
+
+import java.util.DoubleSummaryStatistics;
+
 public class Primitive2Wrapper {
+    public static void main(String[] args) {
+        Boolean b=new Boolean("TRue");
+        System.out.println(b.booleanValue());
+        Long l=new Long("1234");
+        System.out.println(l.intValue());
+        Double d=new Double("12.534");
+        System.out.println(d.doubleValue());
+
+    }
 }
