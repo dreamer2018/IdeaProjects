fa97e8c67cfbe8c30d3c8b3703bd5047781d1359
dream2018 <zhoupans_mail@163.com>
2015-11-17 20:23:35 +0800

    add

--- Exercise/src/Chapter6/src/StringTest.java
+++ Exercise/src/Chapter6/src/StringTest.java
@@ -2,4 +2,12 @@
  * Created by zhoupan on 15-11-16 上午9:29.
  */
 public class StringTest {
+    public static void main(String[] args) {
+        String str1= new String("hello");
+        String str2= new String("hello");
+        System.out.println(str1==str2);
+        System.out.println(str1.equals(str2));
+        System.out.println(str1.hashCode());
+        System.out.println(str2.hashCode());
+    }
 }
