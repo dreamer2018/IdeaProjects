e816408bf55073d99c02df6ea88555883a23991a
dream2018 <zhoupans_mail@163.com>
2015-11-17 22:22:23 +0800

    add

--- Experiment/src/MatrixAdd.java
+++ Experiment/src/MatrixAdd.java
@@ -1,16 +1,31 @@
 /**
  * Created by zhoupan on 15-11-17 下午9:22.
  */
+import java.util.Scanner;
+
 class Matrix{
 
+    //矩阵的长
+    private int x;
+    //矩阵的宽
+    private int y;
     //创建一个矩阵
     private int[][] matrix=new int[3][3];
 
-    //矩阵的set方法，用于初始化矩阵
-    public void setMatrix(int[][] matrix1) {
-        this.matrix = matrix1;
+    //矩阵的无参构造器，默认为3*3矩阵
+    public Matrix(){
+        this.x=3;
+        this.y=3;
+    }
+    //矩阵的有参构造器
+    public Matrix(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
+    public void setMatrix(int[][] matrix){
+        this.matrix=matrix;
+    }
     //矩阵的get方法，用于获取矩阵
     public int[][] getMatrix() {
         return matrix;
@@ -18,7 +33,71 @@ class Matrix{
 }
 
 public class MatrixAdd {
+
+    public int[][] addMatrix(int[][] matrix1, int[][] matrix2 ){
+
+        int[][] temp= new int[matrix1.length][matrix1.length];
+        for (int i = 0; i <temp.length; i++) {
+               for(int j=0;j<temp[i].length;j++){
+                   temp[i][j]=matrix1[i][j]+matrix2[i][j];
+               }
+        }
+        return temp;
+    }
+
+    public void printResult(int[][] a, int[][] b, int[][] c){
+        for (int i = 0; i <a.length ; i++) {
+            for (int j = 0; j <a[a.length].length ; j++) {
+                System.out.println(a[i][j]+);
+            }
+            if(i==1){
+                System.out.println("\t+\t");
+            }else {
+                System.out.println("\t\t");
+            }
+            for (int j = 0; j <a[a.length].length ; j++) {
+                System.out.println(a[i][j]);
+            }
+            if(i==1){
+                System.out.println("\t=\t");
+            }else {
+                System.out.println("\t\t");
+            }
+            for (int j = 0; j <c[c.length].length ; j++) {
+                System.out.println(c[i][j]);
+            }
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("hello world");
+
+        Scanner s=new Scanner(System.in);
+        MatrixAdd ma=new MatrixAdd();
+
+        int[][] array1=new int[3][3];
+        int[][] array2=new int[3][3];
+        int[][] result=new int[3][3];
+
+        for(int i=0;i<array1.length;i++){
+            for (int j=0; j<array1[array1.length].length; j++){
+                array1[i][j]=s.nextInt();
+            }
+        }
+
+        for(int i=0;i<array2.length;i++){
+            for (int j=0; j<array2[array2.length].length; j++){
+                array2[i][j]=s.nextInt();
+            }
+        }
+
+        //创建两个矩阵对象
+        Matrix m1=new Matrix(3,3);
+        Matrix m2=new Matrix(3,3);
+
+        //初始化两个矩阵
+        m1.setMatrix(array1);
+        m2.setMatrix(array2);
+
+        result=ma.addMatrix(m1.getMatrix(),m2.getMatrix());
+        ma.printResult(m1.getMatrix(),m2.getMatrix(),result);
     }
 }
