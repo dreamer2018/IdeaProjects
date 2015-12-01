263c39502771721cef1d676119605b2ae3cb41a1
dreamer2018 <zhoupans_mail@163.com>
2015-12-01 11:03:35 +0800

    add

---
+++ CrazyJava/src/DiscernVariable.java
@@ -0,0 +1,25 @@
+/**
+ * Created by zhoupan on 12/1/15.
+ */
+public class DiscernVariable {
+    private String prop="外部类的实例变量";
+
+    private class InnerClass{
+        private String prop="内部类的实例变量";
+        public void printInfo(){
+            String prop="局部变量的属性值";
+            System.out.printf("外部类的属性值：%s\n", DiscernVariable.this.prop);
+            System.out.printf("内部类的属性值：%s\n",this.prop);
+            System.out.printf("局部变量的属性值：%s\n",prop);
+        }
+    }
+    public void test(){
+        InnerClass ic = new InnerClass();
+        ic.printInfo();
+    }
+
+    public static void main(String[] args) {
+        new DiscernVariable().test();
+    }
+}
+
