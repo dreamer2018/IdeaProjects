265e89ad93974c8cd74f2e2e961be76e497791b2
dreamer2018 <zhoupans_mail@163.com>
2015-12-02 22:32:25 +0800

    add

---
+++ CrazyJava/src/ThrowTest.java
@@ -0,0 +1,24 @@
+/**
+ * Created by zhoupan on 12/2/15.
+ */
+public class ThrowTest {
+    public static void main(String[] args) {
+        try {
+            throwChecked(3);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        throwRuntime(-3);
+    }
+    public static void throwChecked(int a) throws Exception{
+        if(a>0)
+        {
+            throw new Exception("a的值大于0，不符合要求");
+        }
+    }
+    public static void throwRuntime(int a){
+        if(a>0){
+            throw new RuntimeException("a的值大于0，不符合要求");
+        }
+    }
+}
