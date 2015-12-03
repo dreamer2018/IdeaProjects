1709a3ac802fbedde910a45b236038316ae88181
dreamer2018 <zhoupans_mail@163.com>
2015-12-03 22:29:26 +0800

    add

--- CrazyJava/src/PrintStackTraceTest.java
+++ CrazyJava/src/PrintStackTraceTest.java
@@ -23,7 +23,6 @@ public class PrintStackTraceTest {
     public static void secondMethod(){
         thirdMethod();
     }
-
     public static void thirdMethod(){
         throw new SelfException("自定义异常信息");
     }
