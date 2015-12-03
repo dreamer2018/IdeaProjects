46fe8c1718a4d3d89a75423be008c6b0c4cebe17
dreamer2018 <zhoupans_mail@163.com>
2015-12-03 22:18:06 +0800

    add

---
+++ CrazyJava/src/PrintStackTraceTest.java
@@ -0,0 +1,30 @@
+/**
+ * Created by zhoupan on 12/3/15.
+ */
+class SelfException extends RuntimeException{
+
+    SelfException(){
+
+    }
+    SelfException(String msg){
+        super(msg);
+    }
+}
+public class PrintStackTraceTest {
+
+    public static void main(String[] args) {
+        firstMethod();
+    }
+
+    public static void firstMethod(){
+        secondMethod();
+    }
+
+    public static void secondMethod(){
+        thirdMethod();
+    }
+
+    public static void thirdMethod(){
+        throw new SelfException("自定义异常信息");
+    }
+}
