61ddd5e777fc6ecb0b04d2cdc3edcfccac14d795
dreamer2018 <zhoupans_mail@163.com>
2015-12-10 09:20:40 +0800

    debug

--- CrazyJava/src/CreateStaticInnerInstance.java
+++ CrazyJava/src/CreateStaticInnerInstance.java
@@ -1,5 +1,17 @@
 /**
  * Created by zhoupan on 12/10/15.
  */
+class Out {
+    static class Inn {
+        public Inn() {
+            System.out.println("Inn class construct");
+        }
+    }
+}
 public class CreateStaticInnerInstance {
+    public static void main(String[] args) {
+        //Out.Inn in;
+        //in=new Out.Inn();
+        Out.Inn in =new Out.Inn();
+    }
 }
