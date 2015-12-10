9bd9fe432d57adc5d1b6a73b0033277b42b3eb08
dreamer2018 <zhoupans_mail@163.com>
2015-12-10 20:46:08 +0800

    add

--- CrazyJava/src/AWT/ScrollPaneTest.java
+++ CrazyJava/src/AWT/ScrollPaneTest.java
@@ -1,7 +1,18 @@
 package AWT;
 
+import java.awt.*;
+
 /**
  * Created by zhoupan on 12/10/15.
  */
 public class ScrollPaneTest {
+    public static void main(String[] args) {
+        Frame f=new Frame("测试窗口");
+        ScrollPane sp=new ScrollPane(ScrollPane.SCROLLBARS_ALWAYS);
+        sp.add(new TextField(20));
+        sp.add(new Button("单击我"));
+        f.add(sp);
+        f.setBounds(500,500,250,120);
+        f.setVisible(true);
+    }
 }
