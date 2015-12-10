8344b1112642bf7584bb8dfb804819d243b5d924
dreamer2018 <zhoupans_mail@163.com>
2015-12-10 20:03:12 +0800

    add

--- CrazyJava/src/TeachableProgramer.java
+++ CrazyJava/src/TeachableProgramer.java
@@ -2,4 +2,9 @@
  * Created by zhoupan on 12/10/15.
  */
 public class TeachableProgramer {
+    public static void main(String[] args) {
+        TeachableProgrammer tp= new TeachableProgrammer("Jack");
+        tp.work();
+        tp.getCallbackReference().work();
+    }
 }
