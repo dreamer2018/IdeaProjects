1fce6bfb70b72927f71f53e0d6edef13177ea5f6
dreamer2018 <zhoupans_mail@163.com>
2015-12-10 20:45:41 +0800

    add

--- CrazyJava/src/AWT/PanelTest.java
+++ CrazyJava/src/AWT/PanelTest.java
@@ -1,7 +1,18 @@
 package AWT;
 
+import java.awt.*;
+
 /**
  * Created by zhoupan on 12/10/15.
  */
 public class PanelTest {
+    public static void main(String[] args) {
+        Frame f=new Frame("测试窗口");
+        Panel p=new Panel();
+        p.add(new TextField(20));
+        p.add(new Button("单击我"));
+        f.add(p);
+        f.setBounds(500,500,250,200);
+        f.setVisible(true);
+    }
 }
