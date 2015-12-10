96ea7c0fcb0da9c6457d367c8baf1a5d79e1d1f6
dreamer2018 <zhoupans_mail@163.com>
2015-12-10 09:38:36 +0800

    ADD

--- CrazyJava/src/LocalInnerClass.java
+++ CrazyJava/src/LocalInnerClass.java
@@ -2,4 +2,16 @@
  * Created by zhoupan on 12/10/15.
  */
 public class LocalInnerClass {
+    public static void main(String[] args) {
+        class InnerSuper{
+            int a;
+        }
+        class InnerSuber extends InnerSuper{
+            int b;
+        }
+        InnerSuber is=new InnerSuber();
+        is.a=100;
+        is.b=20;
+        System.out.printf("a=%d,b=%d",is.a,is.b);
+    }
 }
