481ce5bf818db1d094fd86b49bfc2e21df046eee
dreamer2018 <zhoupans_mail@163.com>
2015-12-16 19:25:45 +0800

    add

--- CrazyJava/src/MultiThread/SecondThread.java
+++ CrazyJava/src/MultiThread/SecondThread.java
@@ -3,5 +3,22 @@ package MultiThread;
 /**
  * Created by zhoupan on 15-12-16.
  */
-public class SecondThread {
+public class SecondThread implements Runnable{
+    private int i;
+    public void run(){
+        for (;i<100;i++){
+            System.out.println(Thread.currentThread().getName()+" "+i);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i <30; i++) {
+            System.out.println(Thread.currentThread().getName()+" "+i);
+            if(i==20){
+                SecondThread st= new SecondThread();
+                new Thread(st,"线程1").start();
+                new Thread(st,"线程2").start();
+            }
+        }
+    }
 }
