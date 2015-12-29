2474534b15d1e521d25344d29d0aba57422e857d
dreamer2018 <zhoupans_mail@163.com>
2015-12-29 18:30:49 +0800

    add

--- XUPTACM/src/DateConversion.java
+++ XUPTACM/src/DateConversion.java
@@ -1,5 +1,16 @@
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+import java.util.Date;
+
 /**
  * Created by zhoupan on 15-12-29.
  */
 public class DateConversion {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        long time=scanner.nextLong();
+        Date date = new Date(time);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日 HH时mm分ss秒");
+        System.out.print(sdf.format(date));
+    }
 }
