67987e88183d8b7018754af8fd14a3072258a194
dreamer2018 <zhoupans_mail@163.com>
2016-01-07 20:38:27 +0800

    exam

--- XUPTACM/src/JAVXEXAM/sortName.java
+++ XUPTACM/src/JAVXEXAM/sortName.java
@@ -3,5 +3,44 @@ package JAVXEXAM;
 /**
  * Created by zhoupan on 16-1-7.
  */
+import java.util.Scanner;
+
 public class sortName {
+    public Comparable[] Sort(Comparable[] name){
+        Comparable c=null;
+        int k;
+        for (int i = name.length-1; i>=0 ; i--) {
+            k=i;
+            for (int j = 0 ; j < i ; j++) {
+                if(name[k].compareTo(name[j]) < 0){
+                    k=j;
+                }
+            }
+            if (k!=i) {
+                c = name[i];
+                name[i] = name[k];
+                name[k] = c;
+            }
+        }
+        return name;
+    }
+
+    public static void main(String[] args) {
+        sortName ns= new sortName();
+        Scanner s= new Scanner(System.in);
+        int n=s.nextInt();
+        Comparable[] name= new Comparable[n];
+        for (int i = 0; i < n; i++) {
+            name[i]=s.next();
+        }
+        name=ns.Sort(name);
+        for (int i=0;i<n;i++){
+            if(i<n-1){
+                System.out.println(name[i]);
+            }
+            else {
+                System.out.print(name[i]);
+            }
+        }
+    }
 }
