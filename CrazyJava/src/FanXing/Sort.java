52c3c3a269f1e75cf4938dacf853fa129b006ed0
dreamer2018 <zhoupans_mail@163.com>
2016-01-08 14:07:25 +0800

    add

--- CrazyJava/src/FanXing/Sort.java
+++ CrazyJava/src/FanXing/Sort.java
@@ -3,5 +3,29 @@ package FanXing;
 /**
  * Created by zhoupan on 16-1-8.
  */
-public class Sort {
+
+public class Sort{
+    public static <E extends Comparable<E>> void selectionSort(E[] list){
+        int n;
+        E tmp;
+        for (int i = 0; i < list.length; i++) {
+            n=i;
+            for (int j = i+1; j < list.length; j++) {
+                if(list[n].compareTo(list[j])>0){
+                    n=j;
+                }
+            }
+            tmp=list[n];
+            list[n]=list[i];
+            list[i]=tmp;
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[]  a = {2,4,3,5,8,1,7};
+        Sort.selectionSort(a);
+        for(Integer i : a){
+            System.out.printf("%d\t",i);
+        }
+    }
 }
