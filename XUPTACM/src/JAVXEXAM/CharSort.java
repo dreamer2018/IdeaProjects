44a8b4e9f56691adf3f20cca265a14ca4be0e2ee
dreamer2018 <zhoupans_mail@163.com>
2016-01-08 18:13:40 +0800

    exam 4

--- XUPTACM/src/JAVXEXAM/CharSort.java
+++ XUPTACM/src/JAVXEXAM/CharSort.java
@@ -1,7 +1,39 @@
 package JAVXEXAM;
 
+import java.util.Scanner;
 /**
  * Created by zhoupan on 16-1-8.
  */
 public class CharSort {
+    public void Sort(String name){
+        int k;
+        char[] c = name.toCharArray();
+        for (int i = 0; i<name.length() ; i++) {
+            k=i;
+            for (int j =i+1 ; j < name.length() ; j++) {
+                if( c[k]>c[j]){
+                    k=j;
+                }
+            }
+            if (k!=i) {
+                char  ch = c[i];
+                c[i]=c[k];
+                c[k]=ch;
+            }
+        }
+        name=name.valueOf(c);
+        System.out.print(name);
+    }
+    public static void main(String[] args) {
+        CharSort cs= new CharSort();
+        int n,i;
+        Scanner s= new Scanner(System.in);
+        String[] name = new String[100];
+        for (i = 0; i < 100; i++) {
+            if(s.hasNext()){
+                System.out.println();
+            }
+            cs.Sort(s.next());
+        }
+    }
 }
