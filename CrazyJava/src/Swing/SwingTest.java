b482e53526e6fd98522395dfd663daaa963be345
dreamer2018 <zhoupans_mail@163.com>
2016-03-09 22:25:39 +0800

    add

--- CrazyJava/src/Swing/SwingTest.java
+++ CrazyJava/src/Swing/SwingTest.java
@@ -1,7 +1,11 @@
 package Swing;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.applet.Applet;
+import java.applet.AppletContext;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 /**
  * Created by zhoupan on 16-3-9.
@@ -10,6 +14,8 @@ public class SwingTest {
     public void init(){
         JFrame f = new JFrame("TEST");
         f.setSize(800,400);
+        Image img = Toolkit.getDefaultToolkit().createImage("Close12.gif");
+        f.setIconImage(img);
         f.setLayout(new BorderLayout());
         f.add(new JButton("南"),BorderLayout.EAST);
         //f.add(new JColorChooser(),BorderLayout.NORTH);
