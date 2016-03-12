58638eac33c48a70a27dc5bf9651d790b77618ad
dreamer2018 <zhoupans_mail@163.com>
2016-03-12 17:09:06 +0800

    Swing

--- CrazyJava/src/Swing/SwingTest.java
+++ CrazyJava/src/Swing/SwingTest.java
@@ -1,11 +1,7 @@
 package Swing;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.applet.Applet;
-import java.applet.AppletContext;
 import java.awt.*;
-import java.awt.image.BufferedImage;
 
 /**
  * Created by zhoupan on 16-3-9.
@@ -13,13 +9,27 @@ import java.awt.image.BufferedImage;
 public class SwingTest {
     public void init(){
         JFrame f = new JFrame("TEST");
+        JDialog d = new JDialog();
+        d.add(new JButton());
+        d.setVisible(true);
         f.setSize(800,400);
+        //获取图片
         Image img = Toolkit.getDefaultToolkit().createImage("Close12.gif");
+        //设置任务栏图片
         f.setIconImage(img);
+        //设置任务栏名字
+        f.setTitle("Hello");
+        //设置是否关闭,0不关闭，大于0，关闭
+        //f.setDefaultCloseOperation(0);
+
+        f.setResizable(true);
+        //设置布局格式
         f.setLayout(new BorderLayout());
+
         f.add(new JButton("南"),BorderLayout.EAST);
         //f.add(new JColorChooser(),BorderLayout.NORTH);
         //f.add(new JFileChooser(),BorderLayout.WEST);
+
         f.add(new JCheckBox(),BorderLayout.CENTER);
         f.add(new JComboBox<String>());
         f.setVisible(true);
