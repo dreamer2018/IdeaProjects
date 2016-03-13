ce4c08f807ad487c3746c7affd9d93b6a18ad1e1
dreamer2018 <zhoupans_mail@163.com>
2016-03-13 16:47:09 +0800

    AWT

---
+++ CrazyJava/src/AWT/DialogTest.java
@@ -0,0 +1,42 @@
+package AWT;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Administrator on 2016/3/13.
+ */
+public class DialogTest {
+    Frame f =new Frame("测试");
+    Dialog d1 = new Dialog(f,"模式对话框",true);
+    Dialog d2 = new Dialog(f,"非模式对话框",false);
+    Button b1 = new Button("打开模式对话框");
+    Button b2 = new Button("打开非模式对话框");
+    public void init(){
+        d1.setBounds(20,30,300,400);
+        d2.setBounds(20,30,300,400);
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                d1.setVisible(true);
+            }
+        });
+        b2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                d2.setVisible(true);
+            }
+        });
+        f.add(b1);
+        f.add(b2,BorderLayout.SOUTH);
+        f.pack();
+        f.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new DialogTest().init();
+    }
+
+
+}
