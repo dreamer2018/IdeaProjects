5d1a19ec0c0e74cb4bc427afe4501cb48f37d37b
dreamer2018 <zhoupans_mail@163.com>
2016-03-13 17:51:47 +0800

    AWT

--- CrazyJava/src/AWT/EventQs.java
+++ CrazyJava/src/AWT/EventQs.java
@@ -1,8 +1,7 @@
 package AWT;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 /**
  * Created by zhoupan on 16-3-13.
@@ -12,7 +11,9 @@ public class EventQs {
     private Button ok = new Button("确定");
     private TextField tf = new TextField(30);
     public void init(){
-        ok.addActionListener(new okListener());
+       // ok.addActionListener(new okListener());
+       // ok.addFocusListener( new okfocus());
+        ok.addMouseListener(new okmouse());
         f.add(tf);
         f.add(ok,BorderLayout.SOUTH);
         f.pack();
@@ -23,6 +24,52 @@ public class EventQs {
         public void actionPerformed(ActionEvent e) {
             System.out.println("用户单击了ok按钮");
             tf.setText("Hello world");
+        }
+    }
+    class okfocus implements FocusListener{
+
+        @Override
+        public void focusGained(FocusEvent e) {
+            System.out.println("用户进入ok按钮");
+            tf.setText("Hello world——1");
+        }
+
+        @Override
+        public void focusLost(FocusEvent e) {
+            System.out.println("用户离开ok按钮");
+            tf.setText("Hello world--2");
+        }
+    }
+    class okmouse implements MouseListener{
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            System.out.println("用户单击了ok按钮");
+            tf.setText("Hello world-2");
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            System.out.println("用户按压了ok按钮");
+            tf.setText("Hello world-3");
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            System.out.println("用户释放了ok按钮");
+            tf.setText("Hello world-4");
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            System.out.println("用户进入ok按钮");
+            tf.setText("Hello world-5");
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            System.out.println("用户离开了ok按钮");
+            tf.setText("Hello world-6");
         }
     }
 
