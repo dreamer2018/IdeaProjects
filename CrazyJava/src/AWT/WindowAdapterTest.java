0f04903c82185476e677a645bd1c8f2c13730b64
dreamer2018 <zhoupans_mail@163.com>
2016-03-16 21:46:37 +0800

    AWT

--- CrazyJava/src/AWT/WindowAdapterTest.java
+++ CrazyJava/src/AWT/WindowAdapterTest.java
@@ -1,8 +1,30 @@
 package AWT;
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 /**
  * Created by zhoupan on 16-3-15.
  */
 public class WindowAdapterTest {
+    Frame f = new Frame("测试窗口");
+    TextArea ta = new TextArea(6,40);
+    private void init(){
+        f.addWindowListener(new MyWindowsListener());
+        f.add(ta);
+        f.pack();
+        f.setVisible(true);
+    }
+    class  MyWindowsListener extends WindowAdapter{
+        public void windowClosing(WindowEvent e){
+            int i=JOptionPane.showConfirmDialog(null,null);
+            if(i == 1){
 
+            }
+            System.exit(0);
+        }
+    }
+    public static void main(String[] args) {
+        new WindowAdapterTest().init();
+    }
 }
