f35680b42c8063c4623ff5a08216a26202b85065
dreamer2018 <zhoupans_mail@163.com>
2016-03-20 18:32:42 +0800

    add

--- CrazyJava/src/Swing/Tab.java
+++ CrazyJava/src/Swing/Tab.java
@@ -10,16 +10,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-/**
- * Created by zhoupan on 16-3-17.
- */
 
 
-class addTab implements MouseListener {
+class addTab {
     JPanel jp;
     JLabel lab ;
     JLabel lab3=new JLabel();
-    public addTab(){
+    public void addTabs(){
         lab= new JLabel("选项卡1");
         JLabel lab1 = new JLabel("选项卡");
         jp=new JPanel();
@@ -31,57 +28,68 @@ class addTab implements MouseListener {
         jp.add(lab3);
         Tab.pane.addTab("i",lab);
         Tab.pane.setTabComponentAt(Tab.pane.indexOfComponent(lab),jp);//实现这个功能的就这一条最重要的语句
-        lab3.addMouseListener(this);
+        lab3.addMouseListener(new mouseListener());
     }
-    @Override
-    public void mouseClicked(MouseEvent arg0) {
-        // TODO Auto-generated method stub
-        Tab.pane.remove(Tab.pane.indexOfTabComponent(jp));
-    }
-    @Override
-    public void mouseEntered(MouseEvent e) {
-        // TODO Auto-generated method stub
-        lab3.setText("x ");
-    }
-    @Override
-    public void mouseExited(MouseEvent arg0) {
-        // TODO Auto-generated method stub
-        lab3.setText("");
-    }
-    @Override
-    public void mousePressed(MouseEvent arg0) {
-        // TODO Auto-generated method stub
-    }
-    @Override
-    public void mouseReleased(MouseEvent arg0) {
-        // TODO Auto-generated method stub
+    class mouseListener implements MouseListener {
+        @Override
+        public void mouseClicked(MouseEvent arg0) {
+            // TODO Auto-generated method stub
+            Tab.pane.remove(Tab.pane.indexOfTabComponent(jp));
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            // TODO Auto-generated method stub
+            lab3.setText("x ");
+        }
+
+        @Override
+        public void mouseExited(MouseEvent arg0) {
+            // TODO Auto-generated method stub
+            lab3.setText("");
+        }
+
+        @Override
+        public void mousePressed(MouseEvent arg0) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent arg0) {
+            // TODO Auto-generated method stub
+        }
     }
 }
-public class Tab extends JFrame implements ActionListener {
+public class Tab{
     JMenuItem mi;
     static JTabbedPane pane;
-    public Tab() {
-        super("选项卡窗格");
-        setVisible(true);
-        setSize(800, 600);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    JFrame jf = new JFrame("选项卡窗格");
+    public void NewTab() {
+        jf.setVisible(true);
+        jf.setSize(800, 600);
+        jf.setLocationRelativeTo(null);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mi = new JMenuItem("增加");
         JMenu menu = new JMenu("增加");
         menu.add(mi);
         JMenuBar bar = new JMenuBar();
         bar.add(menu);
-        setJMenuBar(bar);
+        jf.setJMenuBar(bar);
         pane = new JTabbedPane();
-        add(pane);
-        new addTab();
-        mi.addActionListener(this);
+        jf.add(pane);
+        new addTab().addTabs();
+        mi.addActionListener(new actionListener());
     }
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-        if(e.getSource()==mi){
-            new addTab();
+    class actionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // TODO Auto-generated method stub
+            if (e.getSource() == mi) {
+                new addTab().addTabs();
+            }
         }
+    }
+    public static void main(String[] args) {
+        new Tab().NewTab();
     }
 }
---
+++ CrazyJava/src/Swing/setSeat.java
@@ -0,0 +1,7 @@
+package Swing;
+
+/**
+ * Created by zhoupan on 16-3-18.
+ */
+public class setSeat {
+}
