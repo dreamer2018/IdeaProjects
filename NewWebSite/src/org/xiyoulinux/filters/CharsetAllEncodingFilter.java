56c24b6f7080babefc630892b0cd59fc4ae58581
dreamer2018 <zhoupans_mail@163.com>
2016-10-30 19:33:56 +0800

    添加编码过滤

--- NewWebSite/src/org/xiyoulinux/filters/CharsetAllEncodingFilter.java
+++ NewWebSite/src/org/xiyoulinux/filters/CharsetAllEncodingFilter.java
@@ -1,42 +1,35 @@
 package org.xiyoulinux.filters;
 
-import java.io.IOException;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 /**
  * Servlet Filter implementation class CharsetAllEncodingFilter
  */
 
-public class CharsetAllEncodingFilter implements Filter
-{
+@WebFilter(filterName = "CharsetAllEncodingFilter", urlPatterns = "/admin/", initParams = {@WebInitParam(name = "encoding", value = "UTF-8")})
+public class CharsetAllEncodingFilter implements Filter {
 
     /**
      * Default constructor.
      */
     private String encoding = null;
 
-    public void init(FilterConfig filterConfig) throws ServletException
-    {
+    public void init(FilterConfig filterConfig) throws ServletException {
         encoding = filterConfig.getInitParameter("encoding");
 
     }
 
-    public void destroy()
-    {}
+    public void destroy() {
+    }
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException
-    {
+            throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
-        if(httpRequest.getMethod().equalsIgnoreCase("POST"))
-        {
+        if (httpRequest.getMethod().equalsIgnoreCase("POST")) {
             request.setCharacterEncoding(encoding);
         }
         chain.doFilter(request, response);
